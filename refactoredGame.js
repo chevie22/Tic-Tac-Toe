3910b2c8cab696c388586f56eddeb9a1f7f0f132
Chevie <179885376+chevie22@users.noreply.github.com>
2024-10-23 23:21:15 +0800

    Finish game logic and add little frontend UI

--- refactoredGame.js
+++ refactoredGame.js
@@ -1,4 +1,4 @@
-function Gameboard(){
+const Gameboard = (function() {
     let board = [];
 
     for(let i = 0; i < 3; i++){
@@ -27,12 +27,72 @@ function Gameboard(){
         getBoard,
         addPlayerMove,
     };
+})();
+
+function ShowGame(){
+
+    let boardArray = Gameboard.getBoard();
+
+    let gameEnd;
+
+    let currentPlayerSymbol;
+
+    let cellsInBoard = 0;
+
+    let gameContainer = document.querySelector("#gameContainer");
+
+
+    const updateBoard = () =>{
+        for(let i = 0; i < boardArray.length; i++){
+            for(let j = 0; j < boardArray[i].length; j++){
+                document.querySelector(`#a${i}a${j}`).innerHTML = boardArray[i][j];
+            }
+        }
+
+    }
+    return{
+        updateBoard,
+    }
 };
 
+(function GameListener(){
+    const gameController = GameController();
+    const showGame = ShowGame();
+
+    let boardArray = Gameboard.getBoard();
+
+    for(let i = 0; i < boardArray.length; i++){
+        for(let j = 0; j < boardArray[i].length; j++){
+            let cell = document.createElement("div");
+            cell.className = "cell";
+            cell.id = `a${i}a${j}`;
+            gameContainer.appendChild(cell);
+        }
+    }
+
+    gameContainer.addEventListener("click", (e) =>{
+        gameEnd = gameController.getGameEnd();
+        currentPlayerSymbol = gameController.getCurrentPlayerTurnPlayerSymbol();
+
+        let target = e.target;
+
+        // if(!gameEnd){
+        //     target.innerHTML = currentPlayerSymbol;
+        // }
+        
+        coordinatesClicked = e.target.id.split("a").map(Number);
+
+        gameController.playerMove(coordinatesClicked[1], coordinatesClicked[2]);
+        showGame.updateBoard();
+        // updateBoard();
+    });
+
+})();
+
 function GameController(playerOneName = "Player1", playerTwoName = "Player2"){
-    const board = Gameboard();
     let gameEnd = false;
     let whosPlayerTurn = 1;
+    let gameMoves = 0;
 
     const winningCombinations = [
         [ [0, 0], [0, 1], [0, 2] ],
@@ -67,23 +127,32 @@ function GameController(playerOneName = "Player1", playerTwoName = "Player2"){
     };
 
     const playerMove = (boardX, boardY) =>{
-        console.log(checkWin());
         if(!gameEnd){
-            isMoveSuccessful = board.addPlayerMove(boardX, boardY, getCurrentPlayerTurnPlayerSymbol());
+            isMoveSuccessful = Gameboard.addPlayerMove(boardX, boardY, getCurrentPlayerTurnPlayerSymbol());
             
             if(isMoveSuccessful){
                 switchPlayerTurn();
+                gameMoves++;
             }
-            console.log(board.getBoard());
-            console.log(`Player ${whosPlayerTurn}'s Turn`);
+
+            if(gameMoves >= 9){
+                gameEnd = true;
+                console.log("Game Tied!");
+            }else{
+                console.log(Gameboard.getBoard());
+                console.log(`Player ${whosPlayerTurn}'s Turn`);
+                checkWin();
+            }
         }
         else{
             console.log("Game is over bruh!!!");
         }
+
+        
     };
 
     const checkWin = () =>{
-        boardState = board.getBoard();
+        boardState = Gameboard.getBoard();
         for(let i = 0; i < winningCombinations.length; i++){
             const [a, b, c] = winningCombinations[i];
             if(
@@ -91,29 +160,23 @@ function GameController(playerOneName = "Player1", playerTwoName = "Player2"){
                 boardState[a[0]][a[1]] === boardState[b[0]][b[1]] &&
                 boardState[a[0]][a[1]] === boardState[c[0]][c[1]]
             ){
+                console.log(`${ boardState[a[0]][a[1]] } winz!`);
                 gameEnd = true;
-                return `${ boardState[a[0]][a[1]] } winz!`;
             }
         }
-        return `no winnar yet :C`;
     };
+
+    const getGameEnd = () =>{
+        return gameEnd;
+    }
 
     return {
         playerMove,
+        getCurrentPlayerTurnPlayerSymbol,
+        getGameEnd,
     }
 }
 
-const game = GameController();
 
-
-game.playerMove(0, 0);
-game.playerMove(0, 1);
-game.playerMove(1, 0);
-game.playerMove(1, 1);
-game.playerMove(2, 0);
-game.playerMove(2, 1);
-game.playerMove(2, 1);
-game.playerMove(2, 1);
-game.playerMove(2, 1);
 
 
